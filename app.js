93fa922faf75826298c17c4e113e1d7bbacac2c4
Arsal Azeem <azeemarsal@gmail.com>
2021-01-25 15:51:04 +0500

    a basic post request,getting user data sent in body

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express=require('express');
-
+const bodyParser = require('body-parser');
 
 const app=express();
+app.use(bodyParser.json());
 app.get('/', function (req, res) {
-    res.send('Hello World')
-  })
-  app.post('/fetch/collections', function (req, res) {
-   console.log(req.params);
+    res.send('This is get end point')
+  });
+// let myFunction = (req, res, next) => {
+//   console.log('i am hererer', req.body);
+//   return next();
+// };
+// let responseFunction = (req, res, next) => {
+//   res.json({ data: req.body });
+// };
+  app.post('/fetch/collections',(req,res)=>{
+  if (req.body["name"]=="arsal")
+  res.send("Your name is arsal");
+  else
+  res.send("Your name is something else");
   })
 app.listen(3000,()=>{
   console.log("Listeining on port 300");
