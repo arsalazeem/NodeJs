ccf83becd2969b1940fd9f9af0febe8371fa3606
Arsal Azeem <azeemarsal@gmail.com>
2021-01-25 10:59:22 +0500

    post

--- app.js
+++ app.js
@@ -5,5 +5,9 @@ const app=express();
 app.get('/', function (req, res) {
     res.send('Hello World')
   })
-
-app.listen(3000);
+  app.post('/fetch/collections', function (req, res) {
+   console.log(req.params);
+  })
+app.listen(3000,()=>{
+  console.log("Listeining on port 300");
+});
