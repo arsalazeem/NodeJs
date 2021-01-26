f2bf1640557ffccf825f6d54db75fabae2b1d8f7
Arsal Azeem <azeemarsal@gmail.com>
2021-01-26 16:14:17 +0500

    delete and post api

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express=require('express');
 const bodyParser = require('body-parser');
 
 const app=express();
+
+const users=[
+   {id:1,name:"arsal azeem"},
+   {id:2,name:"arsal azeem"},
+   {id:3,name:"arsal azeem"}
+]
 app.use(bodyParser.json());
 app.get('/', function (req, res) {
     res.send('This is get end point')
@@ -13,12 +19,23 @@ app.get('/', function (req, res) {
 // let responseFunction = (req, res, next) => {
 //   res.json({ data: req.body });
 // };
+app.post('/collections/delete',(req,res)=>{
+  console.log('/collections/delete');
+  // (req.body["name"]=="arsal") ? res.send(`Welcome dear ${req.body["name"]}`) : res.send(`welcome ${req.body["name"]}`)
+  const found = users.find(num => num["id"] == req.body["id"]);
+ if (found){
+   res.send(found);
+ }
+ else {
+   res.send("The id does not exits");
+ }
+  }); 
   app.post('/fetch/collections',(req,res)=>{
-  if (req.body["name"]=="arsal")
-  res.send("Your name is arsal");
-  else
-  res.send("Your name is something else");
-  })
+  console.log('/fetch/collections');
+  // (req.body["name"]=="arsal") ? res.send(`Welcome dear ${req.body["name"]}`) : res.send(`welcome ${req.body["name"]}`)
+  users.push({name:req.body["name"]});
+  res.send(users);
+  }); 
 app.listen(3000,()=>{
-  console.log("Listeining on port 300");
+  console.log("Listening on port 3000");
 });
